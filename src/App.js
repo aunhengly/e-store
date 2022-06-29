99d331de6018eafe9c6c1d48d948cbdf5385c06c
Hengly Aun <aunhengly@gmail.com>
2022-06-29 16:27:56 -0700

    created checkout page, prodcut summary and functionality of checkout, total price

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/routes/navigation/navigation.component";
 import Shop from "./components/routes/shop/shop.component";
 
+import Checkout from "./components/routes/checkout/checkout.component";
+
 import Home from "./components/routes/home/home.component";
 import "./components/directory/directory.styles.scss";
 
@@ -13,6 +15,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
